139533ba6beb042f2a74f112e33731f000d01909
zul00 <zul.zulkarnaen@gmail.com>
2017-05-11 17:21:27 +0200

    Added function to draw 'ball' and reset initial screen color

--- pingpong.cc
+++ pingpong.cc
@@ -10,11 +10,24 @@
 #include <stdlib.h>
 #include <helix.h>
 
+void draw_ball(coordinate_t p, uint8_t size)
+{
+  // Temporary ball using rectangle
+  fillrect(
+      p.x, p.y,
+      p.x+size, p.y+size,
+      white
+      );
+}
+
 int main(int argc,char** argv)
 {
   printf("Pingpong...\n");
 
   render_init(1);
+
+  // Reset screen with ORANGE 
+  fillrect(0,0,DVI_WIDTH,DVI_HEIGHT,ORANGE);
 
   render_destroy();
 
