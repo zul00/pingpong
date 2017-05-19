0d2e6dad892ec3e50235a03bee509f8b09f55a37
zul00 <zul.zulkarnaen@gmail.com>
2017-05-19 11:56:06 +0200

    Implement FIFO for ball parameter, NEW APPROACH

--- pingpong.cc
+++ pingpong.cc
@@ -12,7 +12,7 @@
 #include <time.h>
 
 #define V_MAX   20
-#define N_BALL  4
+#define N_BALL  1
 #define SIZE    20
 
 #define ERREXIT(str) {fprintf(stderr, "Error: " str "\n"); exit(1);}
@@ -39,6 +39,12 @@ struct ball_t
   color_t color;
 };
 
+CFifo<ball_t,CFifo<>::w> *bwr1;
+CFifo<ball_t,CFifo<>::w> *bwr2;
+
+CFifo<ball_t,CFifo<>::r> *brd1;
+CFifo<ball_t,CFifo<>::r> *brd2;
+
 /**
  * @brief Generate random ball parameter
  * @param p   ball parameter
@@ -46,8 +52,8 @@ struct ball_t
  */
 void generate_ball(ball_t *p)
 {
-  p->pos.x = rand() % DVI_WIDTH;
-  p->pos.y = rand() % DVI_HEIGHT;
+  p->pos.x = rand() % (DVI_WIDTH - p->size);
+  p->pos.y = rand() % (DVI_HEIGHT - p->size);
   p->vel.x = (rand() % (2*V_MAX) - V_MAX);
   p->vel.y = (rand() % (2*V_MAX) - V_MAX);
   p->size  = SIZE;
@@ -120,6 +126,7 @@ void *ping(void *arg)
   // Reset screen with ORANGE 
   fillrect(0, 0, DVI_WIDTH, DVI_HEIGHT, orange);
   render_flip_buffer();
+  fillrect(0, 0, DVI_WIDTH, DVI_HEIGHT, white);
 
   // Initialize random generator
   srand((unsigned) 1*time(&t));
@@ -132,6 +139,7 @@ void *ping(void *arg)
 
   // Check FIFO
   wr->validate();
+  bwr1->validate();
 
   while(1) 
   {
@@ -140,17 +148,18 @@ void *ping(void *arg)
     for (idx=0;idx<N_BALL;idx++)
     {
       update_ball(&(ball[idx]));
-      draw_ball(&(ball[idx]));
+      //draw_ball(&(ball[idx]));
     }
 
-    render_flip_buffer();
+    //render_flip_buffer();
 
-    printf("Ping, ctr=%u, core=%u\n", ctr++, GetProcID());
-    printf("pos%4u;%4u; ", ball[0].pos.x, ball[0].pos.y);
-    printf("vel%4d;%4d\n", ball[0].vel.x, ball[0].vel.y);
+    printf("Ping, ctr=%u, core=%u; ", ctr++, GetProcID());
+    printf("pos%4u;%4u; \n", ball[0].pos.x, ball[0].pos.y);
+    //printf("vel%4d;%4d\n", ball[0].vel.x, ball[0].vel.y);
+    bwr1->push(ball[0]);
     wr->push(true);
 
-    usleep(800000);
+    usleep(500000);
   }
   return NULL;
 }
@@ -173,6 +182,7 @@ void *pong(void *arg)
   // Check FIFO
   rd->validate();
   wr2->validate();
+  bwr2->validate();
 
   while (true)
   {
@@ -182,16 +192,18 @@ void *pong(void *arg)
     for (idx=0;idx<N_BALL;idx++)
     {
       update_ball(&(ball2[idx]));
-      draw_ball(&(ball2[idx]));
+      //draw_ball(&(ball2[idx]));
     }
 
     // Flip buffer
-    render_flip_buffer();
+    //render_flip_buffer();
 
-    printf("\tPong, core=%u\n", GetProcID());
-    printf("pos%4u;%4u; ", ball2[0].pos.x, ball2[0].pos.y);
-    printf("vel%4d;%4d\n", ball2[0].vel.x, ball2[0].vel.y);
+    printf("\tPong, core=%u", GetProcID());
+    printf("pos%4u;%4u; \n", ball2[0].pos.x, ball2[0].pos.y);
+    //printf("vel%4d;%4d\n", ball2[0].vel.x, ball2[0].vel.y);
+    //bwr2->push(ball[0]);
     wr2->push(true);
+    usleep(1000);
   }
 
   render_destroy();
@@ -202,8 +214,11 @@ void *pong(void *arg)
 void *bing(void *arg) 
 {
   ball_t ball3[N_BALL];
+  ball_t ball1;
   uint8_t idx = 0;
   time_t t;
+  //timestamp_t ts;
+  //timestamp_start(&ts);
 
   // Initialize random generator
   srand((unsigned) 3*time(&t));
@@ -216,10 +231,13 @@ void *bing(void *arg)
 
   // Check FIFO
   rd2->validate();
+  brd1->validate();
 
   while (true)
   {
     rd2->pop();
+    ball1 = brd1->front();
+    brd1->pop();
 
     // Draw to back buffer
     for (idx=0;idx<N_BALL;idx++)
@@ -229,11 +247,15 @@ void *bing(void *arg)
     }
 
     // Flip buffer
-    render_flipnow();
+    render_flip_buffer();
+    usleep(1000);
 
-    printf("\t\tBing, core=%u\n", GetProcID());
-    printf("pos%4u;%4u; ", ball3[0].pos.x, ball3[0].pos.y);
-    printf("vel%4d;%4d\n", ball3[0].vel.x, ball3[0].vel.y);
+    printf("\t\tBing, core=%u;", GetProcID());
+    printf("ball1 = %4u; %4u\n\n", ball1.pos.x, ball1.pos.y);
+    //printf("pos%4u;%4u; ", ball3[0].pos.x, ball3[0].pos.y);
+    //printf("vel%4d;%4d\n", ball3[0].vel.x, ball3[0].vel.y);
+
+    //printf("FPS: %.3f\n",1000.0/(double)timestamp_diff_ms(&ts)*256);
   }
 
   render_destroy();
@@ -250,8 +272,15 @@ int main(int argc,char** argv)
   // Prepare FIFO
   CFifoPtr<bool> fifo12 = CFifo<bool>::Create(1, wr, 2, rd, 2);
   CFifoPtr<bool> fifo23 = CFifo<bool>::Create(2, wr2, 3, rd2, 2);
+
+  CFifoPtr<ball_t> fifo_b13 = CFifo<ball_t>::Create(1, bwr1, 3, brd1, 2);
+  CFifoPtr<ball_t> fifo_b23 = CFifo<ball_t>::Create(2, bwr2, 3, brd2, 2);
+
   if(!fifo12.valid()) ERREXIT("Error creating buffer");
   if(!fifo23.valid()) ERREXIT("Error creating buffer");
+
+  if(!fifo_b13.valid()) ERREXIT("Error creating buffer");
+  if(!fifo_b23.valid()) ERREXIT("Error creating buffer");
 
   // Create process
   if(int e=CreateProcess(pid0, ping, NULL, PROC_DEFAULT_TIMESLICE,
